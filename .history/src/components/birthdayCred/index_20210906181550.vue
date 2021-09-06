991e810773f112627c0d05771cb5a6416281a795
Yaha <mtshen1997@163.com>
2021-09-06 18:22:59 +0800

    1

--- .history/src/components/birthdayCred/index_20210906181550.vue
+++ .history/src/components/birthdayCred/index_20210906181550.vue
@@ -1,15 +1,13 @@
 <template>
-  <div class="credItem">
-    <span>{{ title }}</span>
-    <span>{{ value }}</span>
+  <div>
+
   </div>
 </template>
 
 <script>
 export default {
   props: {
-    title: String,
-    value: String,
+
   },
   data() {
     return {
@@ -38,12 +36,5 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.credItem {
-  border-radius: 4px;
-  border: 1px solid #ebeef5;
-  background-color: #fff;
-  overflow: hidden;
-  color: #303133;
-  transition: .3s;
-}
+
 </style>
